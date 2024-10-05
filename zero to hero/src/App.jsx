1f2087fc9841e44938fab0aea84672cabd703e21
Bibek Biswas <jbiswas94236@gmail.com>
2024-10-05 23:33:04 +0530

    minor changes

--- zero to hero/src/App.jsx
+++ zero to hero/src/App.jsx
@@ -3,9 +3,9 @@ import Cards from "./Cards";
 import BgBtn from "./BgBtn";
 
 function App() {
-  const [color, setColor] = useState("");
-  //
-  const [counter, setCounter] = useState(6);
+  const [color, setColor] = useState(""); //Background Color Changer
+
+  const [counter, setCounter] = useState(6); //Counter Updater
   const addCounter = () => {
     if (counter < 10) {
       setCounter(counter + 1);
@@ -28,42 +28,46 @@ function App() {
 
       <div className="flex flex-col  items-center gap-5">
         <h1 className="text-xl">Rate Me : {counter}</h1>
-        <button
-          onClick={addCounter}
-          className="bg-slate-500 w-20 rounded-md p-1 px-3 hover:bg-slate-400"
-        >
-          Add+
-        </button>
 
-        <button
-          onClick={subCounter}
-          className="bg-slate-500 w-20 rounded-md p-1 px-3 hover:bg-slate-400"
-        >
-          Sub-
-        </button>
+        <div className="flex gap-3">
+          <button
+            onClick={addCounter}
+            className="bg-slate-500 w-9 h-9 rounded-full relative text-sm font-bold  hover:bg-slate-400"
+          >
+            ▲
+          </button>
+
+          <button
+            onClick={subCounter}
+            className="bg-slate-500 w-9 h-9 rounded-full relative text-sm font-bold hover:bg-slate-400"
+          >
+            ▼
+          </button>
+        </div>
       </div>
 
       <h2 className="pl-28 text-2xl font-bold pb-5 pt-14">Dynamic Cards</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-14 gap-x-10 pb-10">
-        <Cards title="Symptom" img="./public/Images/1.jpg" />
-        <Cards title="Peace " img="./public/Images/2.jpg" />
-        <Cards title="Heaven" img="./public/Images/3.jpg" />
-        <Cards title="Pearl" color="red" para="" img="./public/Images/4.jpg" />
         <Cards
-          title="Shallow"
-          para="Float like Balloons"
-          img="./public/Images/5.jpg"
+          title="Symptom"
+          img="./public/Images/1.jpg"
+          para="Tropical Birds"
         />
-        <Cards title="Amuse" para="" img="./public/Images/6.jpg" />
+        <Cards title="Peace" img="./public/Images/2.jpg" para="Swimming Pool" />
+        <Cards title="Heaven" img="./public/Images/3.jpg" para="Mizo Hills" />
+        <Cards title="Pearl" img="./public/Images/4.jpg" para="Elements" />
+        <Cards title="Shallow" img="./public/Images/5.jpg" para="Balloons" />
+        <Cards title="Amuse" img="./public/Images/6.jpg" para="Ride" />
       </div>
 
       <div className="fixed flex flex-wrap justify-around items-center top-8 inset-x-0 px-2 rounded ">
         <div className="max-w-fit flex-wrap bg-slate-800 flex justify-around items-center p-2 rounded-3xl gap-6 drop-shadow-lg">
-          <BgBtn colorName="CadetBlue" setColor={setColor} />
-          <BgBtn colorName="SeaGreen" setColor={setColor} />
-          <BgBtn colorName="Navy" setColor={setColor} />
-          <BgBtn colorName="PaleVioletRed" setColor={setColor} />
+          <BgBtn colorName="SlateBlue" set={setColor} />
+          <BgBtn colorName="SeaGreen" set={setColor} />
+          <BgBtn colorName="RebeccaPurple" set={setColor} />
+          <BgBtn colorName="DarkSlateGray" set={setColor} />
+          <BgBtn colorName="MidnightBlue" set={setColor} />
         </div>
       </div>
     </div>
--- zero to hero/src/BgBtn.jsx
+++ zero to hero/src/BgBtn.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function BgBtn(props) {
+  const BgChange = () => {
+    props.set(props.colorName);
+  };
   return (
     <button
       className="bg-slate-600 rounded-3xl px-3 py-[4px] flex-wrap"
       style={{ backgroundColor: props.colorName }}
-      onClick={() => props.setColor(props.colorName)}
+      onClick={BgChange}
     >
       {props.colorName}
     </button>
